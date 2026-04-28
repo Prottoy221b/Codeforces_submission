40c9105b4505eac4759e36f7ee9caa4cf63d44a4
Nagib Mahmud Prottoy <nagibmahmudprottoy221@gmail.com>
2026-04-28 20:02:29 +0600

    Add 4A - Watermelon solution (800)

--- 800/A_Watermelon.cpp
+++ 800/A_Watermelon.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
+/*
+Problem: 4A - Watermelon
+
+Approach:
+We need to check if the given weight can be split into two EVEN parts.
+Conditions:
+1. Weight must be even
+2. Weight must be greater than 2 (because 2 = 1 + 1, both odd)
+
+So:
+if (weight % 2 == 0 && weight > 2) → YES
+else → NO
+
+Time Complexity: O(1)
+*/
+
 
 
 int main()
